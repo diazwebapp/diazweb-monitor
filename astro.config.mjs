05d81a052065c455a1b7568bc57cfbead78431cd
diazwebapp <diazwebapp@gmail.com>
2025-07-20 23:50:16 +0000

    instalado adapter cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 export const domain = process.env.PUBLIC_DOMAIN;
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    site:domain
+  output: 'server',
+  site:domain,
+  adapter: cloudflare()
 });
