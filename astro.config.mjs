4f92ffc615451fb0468a2be432df1233c68d7861
diazwebapp <diazwebapp@gmail.com>
2025-07-21 01:32:26 +0000

    se cambio el adapter a vercel.... esto petara en cloudflare :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+//import cloudflare from '@astrojs/cloudflare';
+import vercel from '@astrojs/vercel';
 export const domain = process.env.PUBLIC_DOMAIN;
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site:domain,
-  adapter: cloudflare()
+  adapter: vercel()
 });
